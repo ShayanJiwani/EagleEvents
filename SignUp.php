cec456395255cbf15f4a0d683aeac081c01cd3fa
Shayan Jiwani <shayanjiwani@gmail.com>
2018-12-10 04:00:14 -0500

    minor title edit

--- SignUp.php
+++ SignUp.php
@@ -65,7 +65,7 @@ if ($_POST != NULL) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>EagleEvents | Log in</title>
+  <title>EagleEvents | Sign Up</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
